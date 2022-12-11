b676c6ab95fe05602f16e97d850d0b7accb0e36a
Guddyx <55354502+Guddyx@users.noreply.github.com>
2022-12-11 22:48:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git UA2F https://github.com/EOYOHOO/UA2F' >>feeds.conf.default
-#echo 'src-git rkpipid https://github.com/EOYOHOO/rkp-ipid' >>feeds.conf.default
-echo 'src-git campusNetwork https://github.com/FunctionXJB/Campus-network' >>feeds.conf.default
+echo 'src-git passwall https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Lienol/openwrt-package
+# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
