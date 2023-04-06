2d6b1558a925162ee89dc16b1930465c6519f90d
Guddyx <55354502+Guddyx@users.noreply.github.com>
2023-04-06 23:11:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-
-# Lienol/openwrt-package
-# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
-# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
