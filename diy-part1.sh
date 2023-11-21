eae4c1f76d98de5c987a8f59d55b0c7827d20c74
Guddyx <55354502+Guddyx@users.noreply.github.com>
2023-11-22 02:04:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git haiibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
