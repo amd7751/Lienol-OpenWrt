4893ac11bd7f1d0cc426508afe489f2c152245b7
Guddyx <55354502+Guddyx@users.noreply.github.com>
2023-12-10 11:28:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
 #rm -rf feeds/packages/net/mosdns
