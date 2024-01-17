7114ec78f7c0c6f85c6781b5f49ff3a92a72e7ac
Guddyx <55354502+Guddyx@users.noreply.github.com>
2024-01-17 10:07:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,13 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-# 移除要替换的包
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/luci/applications/luci-app-mosdns
-#rm -rf feeds/packages/net/v2ray-geodata
-
-# 添加luci-app-mosdns
-#svn co https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
-#svn co https://github.com/sbwml/luci-app-mosdns/trunk/mosdns feeds/packages/net/mosdns
-#svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-geodata feeds/packages/net/v2ray-geodata
